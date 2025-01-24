c06b5ddc91ee034c48d85cb64901c043a24b7d74
gitautami <utamigita797@gmail.com>
2025-01-24 16:25:24 +0700

    add halaman login

---
+++ tbpbo2/login.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace tbpbo2
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.download;
+            pictureBox1.Location = new Point(403, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(348, 451);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 17;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkRed;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(104, 344);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 49);
+            button1.TabIndex = 16;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(34, 229);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(262, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(34, 131);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(262, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.DarkRed;
+            label4.Location = new Point(34, 208);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 18);
+            label4.TabIndex = 12;
+            label4.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.DarkRed;
+            label3.Location = new Point(34, 107);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 21);
+            label3.TabIndex = 11;
+            label3.Text = "Username";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkRed;
+            label1.Location = new Point(127, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 28);
+            label1.TabIndex = 9;
+            label1.Text = "Welcome!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkRed;
+            label2.Location = new Point(146, 259);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 21);
+            label2.TabIndex = 18;
+            label2.Text = " forget password?";
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(745, 450);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Name = "login";
+            Text = "login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label3;
+        private Label label1;
+        private Label label2;
+    }
+}

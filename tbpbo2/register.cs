2039b7231113391471b03bc6ac988548fd76c2bf
gitautami <utamigita797@gmail.com>
2025-01-24 16:26:25 +0700

    add halaman register

---
+++ tbpbo2/register.cs
@@ -0,0 +1,15 @@
+namespace tbpbo2
+{
+    public partial class register : Form
+    {
+        public register()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

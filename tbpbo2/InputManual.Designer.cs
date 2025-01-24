2df3512c2d5fe2ac0844cdc425b6852d01a87ecb
gitautami <utamigita797@gmail.com>
2025-01-24 17:52:19 +0700

    add form halaman input manual

---
+++ tbpbo2/InputManual.Designer.cs
@@ -0,0 +1,458 @@
+﻿namespace tbpbo2
+{
+    partial class InputManual
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputManual));
+            panel1 = new Panel();
+            buttonInputManual = new Button();
+            buttonLogout = new Button();
+            buttonLaporan = new Button();
+            buttonJadwal = new Button();
+            label1 = new Label();
+            buttonInput = new Button();
+            label8 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label2 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label9 = new Label();
+            comboBoxPasien = new ComboBox();
+            textBoxDenyut = new TextBox();
+            textBoxTekanan = new TextBox();
+            textBoxBerat = new TextBox();
+            textBoxSuhu = new TextBox();
+            dateTimePickerPencatatan = new DateTimePicker();
+            textBoxCatatan = new TextBox();
+            buttonEdit = new Button();
+            buttonSimpan = new Button();
+            dataGridView1 = new DataGridView();
+            comboBoxFilterPasien = new ComboBox();
+            label10 = new Label();
+            label11 = new Label();
+            dateTimePickerDari = new DateTimePicker();
+            dateTimePickerSampai = new DateTimePicker();
+            label12 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(buttonInputManual);
+            panel1.Controls.Add(buttonLogout);
+            panel1.Controls.Add(buttonLaporan);
+            panel1.Controls.Add(buttonJadwal);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(buttonInput);
+            panel1.Location = new Point(3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(144, 612);
+            panel1.TabIndex = 2;
+            // 
+            // buttonInputManual
+            // 
+            buttonInputManual.Font = new Font("Century", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonInputManual.ForeColor = Color.DarkRed;
+            buttonInputManual.Image = (Image)resources.GetObject("buttonInputManual.Image");
+            buttonInputManual.ImageAlign = ContentAlignment.TopCenter;
+            buttonInputManual.Location = new Point(0, 313);
+            buttonInputManual.Name = "buttonInputManual";
+            buttonInputManual.Size = new Size(144, 71);
+            buttonInputManual.TabIndex = 18;
+            buttonInputManual.Text = "Input Manual Kondisi Kesehatan";
+            buttonInputManual.TextAlign = ContentAlignment.BottomCenter;
+            buttonInputManual.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogout
+            // 
+            buttonLogout.BackColor = Color.DarkRed;
+            buttonLogout.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonLogout.ForeColor = Color.White;
+            buttonLogout.Location = new Point(9, 524);
+            buttonLogout.Name = "buttonLogout";
+            buttonLogout.Size = new Size(124, 49);
+            buttonLogout.TabIndex = 17;
+            buttonLogout.Text = "Logout";
+            buttonLogout.UseVisualStyleBackColor = false;
+            // 
+            // buttonLaporan
+            // 
+            buttonLaporan.Font = new Font("Century", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonLaporan.ForeColor = Color.DarkRed;
+            buttonLaporan.Image = (Image)resources.GetObject("buttonLaporan.Image");
+            buttonLaporan.ImageAlign = ContentAlignment.TopCenter;
+            buttonLaporan.Location = new Point(3, 425);
+            buttonLaporan.Name = "buttonLaporan";
+            buttonLaporan.Size = new Size(144, 71);
+            buttonLaporan.TabIndex = 5;
+            buttonLaporan.Text = "Laporan Kesehatan";
+            buttonLaporan.TextAlign = ContentAlignment.BottomCenter;
+            buttonLaporan.UseVisualStyleBackColor = true;
+            // 
+            // buttonJadwal
+            // 
+            buttonJadwal.Font = new Font("Century", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonJadwal.ForeColor = Color.DarkRed;
+            buttonJadwal.Image = (Image)resources.GetObject("buttonJadwal.Image");
+            buttonJadwal.ImageAlign = ContentAlignment.TopCenter;
+            buttonJadwal.Location = new Point(3, 195);
+            buttonJadwal.Name = "buttonJadwal";
+            buttonJadwal.Size = new Size(144, 75);
+            buttonJadwal.TabIndex = 4;
+            buttonJadwal.Text = "Jadwal Kunjungan";
+            buttonJadwal.TextAlign = ContentAlignment.BottomCenter;
+            buttonJadwal.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkRed;
+            label1.Location = new Point(19, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 22);
+            label1.TabIndex = 3;
+            label1.Text = "Dashboard";
+            // 
+            // buttonInput
+            // 
+            buttonInput.Font = new Font("Century", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonInput.ForeColor = Color.DarkRed;
+            buttonInput.Image = (Image)resources.GetObject("buttonInput.Image");
+            buttonInput.ImageAlign = ContentAlignment.TopCenter;
+            buttonInput.Location = new Point(0, 84);
+            buttonInput.Name = "buttonInput";
+            buttonInput.Size = new Size(144, 74);
+            buttonInput.TabIndex = 0;
+            buttonInput.Text = "Input Data Pasien";
+            buttonInput.TextAlign = ContentAlignment.BottomCenter;
+            buttonInput.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Century", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.DarkRed;
+            label8.Location = new Point(167, 122);
+            label8.Name = "label8";
+            label8.Size = new Size(241, 23);
+            label8.TabIndex = 36;
+            label8.Text = "Form Input Data Vital";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.DarkRed;
+            label6.Location = new Point(167, 350);
+            label6.Name = "label6";
+            label6.Size = new Size(147, 18);
+            label6.TabIndex = 35;
+            label6.Text = "Tanggal Pencatatan";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.DarkRed;
+            label7.Location = new Point(167, 315);
+            label7.Name = "label7";
+            label7.Size = new Size(97, 18);
+            label7.TabIndex = 34;
+            label7.Text = "Berat Badan";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkRed;
+            label2.Location = new Point(167, 278);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 18);
+            label2.TabIndex = 33;
+            label2.Text = "Suhu Tubuh";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.DarkRed;
+            label5.Location = new Point(167, 244);
+            label5.Name = "label5";
+            label5.Size = new Size(121, 18);
+            label5.TabIndex = 32;
+            label5.Text = "Denyut Jantung";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.DarkRed;
+            label4.Location = new Point(167, 206);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 18);
+            label4.TabIndex = 31;
+            label4.Text = "Tekanan Darah";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.DarkRed;
+            label3.Location = new Point(167, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 18);
+            label3.TabIndex = 30;
+            label3.Text = "Nama Pasien";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.DarkRed;
+            label9.Location = new Point(167, 384);
+            label9.Name = "label9";
+            label9.Size = new Size(141, 18);
+            label9.TabIndex = 37;
+            label9.Text = "Catatan Tambahan";
+            // 
+            // comboBoxPasien
+            // 
+            comboBoxPasien.FormattingEnabled = true;
+            comboBoxPasien.Location = new Point(323, 162);
+            comboBoxPasien.Name = "comboBoxPasien";
+            comboBoxPasien.Size = new Size(223, 28);
+            comboBoxPasien.TabIndex = 38;
+            // 
+            // textBoxDenyut
+            // 
+            textBoxDenyut.Location = new Point(323, 237);
+            textBoxDenyut.Name = "textBoxDenyut";
+            textBoxDenyut.Size = new Size(223, 27);
+            textBoxDenyut.TabIndex = 40;
+            // 
+            // textBoxTekanan
+            // 
+            textBoxTekanan.Location = new Point(323, 202);
+            textBoxTekanan.Name = "textBoxTekanan";
+            textBoxTekanan.Size = new Size(223, 27);
+            textBoxTekanan.TabIndex = 39;
+            // 
+            // textBoxBerat
+            // 
+            textBoxBerat.Location = new Point(323, 309);
+            textBoxBerat.Name = "textBoxBerat";
+            textBoxBerat.Size = new Size(223, 27);
+            textBoxBerat.TabIndex = 42;
+            // 
+            // textBoxSuhu
+            // 
+            textBoxSuhu.Location = new Point(323, 274);
+            textBoxSuhu.Name = "textBoxSuhu";
+            textBoxSuhu.Size = new Size(223, 27);
+            textBoxSuhu.TabIndex = 41;
+            // 
+            // dateTimePickerPencatatan
+            // 
+            dateTimePickerPencatatan.Location = new Point(320, 344);
+            dateTimePickerPencatatan.Name = "dateTimePickerPencatatan";
+            dateTimePickerPencatatan.Size = new Size(223, 27);
+            dateTimePickerPencatatan.TabIndex = 43;
+            // 
+            // textBoxCatatan
+            // 
+            textBoxCatatan.Location = new Point(320, 380);
+            textBoxCatatan.Name = "textBoxCatatan";
+            textBoxCatatan.Size = new Size(223, 27);
+            textBoxCatatan.TabIndex = 44;
+            // 
+            // buttonEdit
+            // 
+            buttonEdit.BackColor = Color.DarkRed;
+            buttonEdit.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonEdit.ForeColor = Color.White;
+            buttonEdit.Location = new Point(361, 429);
+            buttonEdit.Name = "buttonEdit";
+            buttonEdit.Size = new Size(114, 36);
+            buttonEdit.TabIndex = 46;
+            buttonEdit.Text = "Edit";
+            buttonEdit.UseVisualStyleBackColor = false;
+            // 
+            // buttonSimpan
+            // 
+            buttonSimpan.BackColor = Color.DarkRed;
+            buttonSimpan.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonSimpan.ForeColor = Color.White;
+            buttonSimpan.Location = new Point(225, 429);
+            buttonSimpan.Name = "buttonSimpan";
+            buttonSimpan.Size = new Size(114, 36);
+            buttonSimpan.TabIndex = 45;
+            buttonSimpan.Text = "Simpan";
+            buttonSimpan.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(579, 23);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(656, 475);
+            dataGridView1.TabIndex = 47;
+            // 
+            // comboBoxFilterPasien
+            // 
+            comboBoxFilterPasien.FormattingEnabled = true;
+            comboBoxFilterPasien.Location = new Point(694, 522);
+            comboBoxFilterPasien.Name = "comboBoxFilterPasien";
+            comboBoxFilterPasien.Size = new Size(223, 28);
+            comboBoxFilterPasien.TabIndex = 50;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.DarkRed;
+            label10.Location = new Point(538, 566);
+            label10.Name = "label10";
+            label10.Size = new Size(127, 18);
+            label10.TabIndex = 49;
+            label10.Text = "Rentang Tanggal";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.DarkRed;
+            label11.Location = new Point(538, 526);
+            label11.Name = "label11";
+            label11.Size = new Size(145, 18);
+            label11.TabIndex = 48;
+            label11.Text = "Filter Nama Pasien";
+            // 
+            // dateTimePickerDari
+            // 
+            dateTimePickerDari.Location = new Point(694, 560);
+            dateTimePickerDari.Name = "dateTimePickerDari";
+            dateTimePickerDari.Size = new Size(223, 27);
+            dateTimePickerDari.TabIndex = 51;
+            // 
+            // dateTimePickerSampai
+            // 
+            dateTimePickerSampai.Location = new Point(1012, 560);
+            dateTimePickerSampai.Name = "dateTimePickerSampai";
+            dateTimePickerSampai.Size = new Size(223, 27);
+            dateTimePickerSampai.TabIndex = 52;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Century", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.DarkRed;
+            label12.Location = new Point(935, 566);
+            label12.Name = "label12";
+            label12.Size = new Size(60, 18);
+            label12.TabIndex = 53;
+            label12.Text = "Sampai";
+            // 
+            // InputManual
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1247, 613);
+            Controls.Add(label12);
+            Controls.Add(dateTimePickerSampai);
+            Controls.Add(dateTimePickerDari);
+            Controls.Add(comboBoxFilterPasien);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(dataGridView1);
+            Controls.Add(buttonEdit);
+            Controls.Add(buttonSimpan);
+            Controls.Add(textBoxCatatan);
+            Controls.Add(dateTimePickerPencatatan);
+            Controls.Add(textBoxBerat);
+            Controls.Add(textBoxSuhu);
+            Controls.Add(textBoxDenyut);
+            Controls.Add(textBoxTekanan);
+            Controls.Add(comboBoxPasien);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(panel1);
+            Name = "InputManual";
+            Text = "InputManual";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button buttonInputManual;
+        private Button buttonLogout;
+        private Button buttonLaporan;
+        private Button buttonJadwal;
+        private Label label1;
+        private Button buttonInput;
+        private Label label8;
+        private Label label6;
+        private Label label7;
+        private Label label2;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label9;
+        private ComboBox comboBoxPasien;
+        private TextBox textBoxDenyut;
+        private TextBox textBoxTekanan;
+        private TextBox textBoxBerat;
+        private TextBox textBoxSuhu;
+        private DateTimePicker dateTimePickerPencatatan;
+        private TextBox textBoxCatatan;
+        private Button buttonEdit;
+        private Button buttonSimpan;
+        private DataGridView dataGridView1;
+        private ComboBox comboBoxFilterPasien;
+        private Label label10;
+        private Label label11;
+        private DateTimePicker dateTimePickerDari;
+        private DateTimePicker dateTimePickerSampai;
+        private Label label12;
+    }
+}

e9b97550e26267248445de5168c82388de952ade
gitautami <utamigita797@gmail.com>
2025-02-21 10:26:40 +0700

    ganti dl ke halaman laporan

--- tbpbo2/Program.cs
+++ tbpbo2/Program.cs
@@ -11,7 +11,7 @@ namespace tbpbo2
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new login());
+            Application.Run(new Laporan());
         }
     }
 }
